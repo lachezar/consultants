4ec64203833a4fde716515fc15c925345ccb735e
Lachezar Yankov <lacyan@50342.lan>
2012-07-08 22:51:28 +0200

    some othe shit

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -70,7 +70,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-	'/hellodjango/static',
+	os.getcwd() + '/static',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -111,7 +111,7 @@ ROOT_URLCONF = 'hellodjango.urls'
 WSGI_APPLICATION = 'hellodjango.wsgi.application'
 
 TEMPLATE_DIRS = (
-	'/hellodjango/templates',
+	os.getcwd() + '/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
