bfdf06ab3d7fcd348318a828ab4832c6834e4b73
Lachezar Yankov <lacyan@50342.lan>
2012-07-08 21:23:39 +0200

    stupid heroku

---
+++ github_scrape/utils.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.db import IntegrityError
+from django.utils.encoding import smart_str
+
+from models import GithubUser
+import json
+import operator
+import urllib2
+
+def get_github_users(keyword, page):
+    keyword_lower = smart_str(keyword.lower())
+    url_base = smart_str(u'https://api.github.com/legacy/user/search/%s?start_page=%d')
+    url = url_base % (keyword, page)
+    fp = urllib2.urlopen(url)
+    users = json.load(fp)['users']
+    fields = map(operator.attrgetter('name'), GithubUser._meta._fields())
+    github_users = []
+    for u in users:
+        if keyword_lower in smart_str(u['location'].lower()):
+            u['github_id'] = u['id']
+            del u['id']
+            github_users.append(GithubUser(**dict(filter(lambda x: x[0] in fields, u.iteritems()))))
+        
+    return github_users
+                 
+def scrape_location(keyword):
+    page = 1
+    users = get_github_users(keyword, page)
+    while users:
+        print 'page %d, gonna save %d users' % (page, len(users))
+        for u in users:
+            try:
+                u.save()
+            except IntegrityError:
+                pass
+        page += 1
+        users = get_github_users(keyword, page)
+    
+def scrape_sweden():
+    keywords = map(smart_str, [u'Göteborg', 'Sverige', 'Sweden', 'Stockholm', u'Göteborg', 'Gothenburg',
+    u'Malmö', 'Malmo', 'Uppsala', u'Västerås', u'Örebro', u'Linköping', 'Helsingborg', 
+    u'Norrköping', u'Jönköping', 'Lund', u'Umeå', u'Gävle', u'Borås', u'Södertälje', u'Täby',
+    'Eskilstuna', 'Karlstad', 'Halmstad', u'Växjö'])
+    
+    for k in keywords:
+        print "Trying for ", k
+        scrape_location(k)
+        
+        
---
+++ github_scrape/views.py
@@ -0,0 +1,23 @@
+# Create your views here.
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from models import GithubUser
+
+import operator
+
+def index(request, language=None):
+    if language is None:
+        users = GithubUser.objects.all()
+    else:
+        users = GithubUser.objects.filter(language__iexact=language)
+    languages = GithubUser.objects.values_list('language').exclude(language='').distinct().order_by('language')
+    languages = map(operator.itemgetter(0), languages)
+    
+    return render_to_response('list.html', 
+                              {'users': users, 
+                               'languages': languages, 
+                               'selected_language': language
+                              }, context_instance=RequestContext(request))
+    
+    
