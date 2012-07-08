b6d9f9cb06c2237eb6db780618d78a0e388d9e24
Lachezar Yankov <lacyan@50342.lan>
2012-07-08 23:33:35 +0200

    some othe shit

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -3,7 +3,7 @@
 import os
 import os.path
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -65,11 +65,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/app/hellodjango/static'
+STATIC_ROOT = os.path.dirname(__file__) + '/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = ''
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
