a44d8e5a1183f0997312eae5cb92274c2a2e71b1
Lachezar Yankov <lacyan@50342.lan>
2012-07-09 19:01:24 +0200

    fixed pixels and added tabs

--- github_scrape/views.py
+++ github_scrape/views.py
@@ -9,6 +9,8 @@ import operator
 def index(request, language=None):
     if language is None:
         users = GithubUser.objects.all()
+    elif language == 'None':
+        users = GithubUser.objects.filter(language='')
     else:
         users = GithubUser.objects.filter(language__iexact=language)
     users = users.order_by('-public_repo_count')
