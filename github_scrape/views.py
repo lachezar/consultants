88c194e394f35fee580d4208167ab74067bb9a7f
Lachezar Yankov <lucho870601@gmail.com>
2012-07-31 01:15:25 +0200

    github has changed the api response... thier api is aweful

--- github_scrape/views.py
+++ github_scrape/views.py
@@ -10,11 +10,11 @@ def index(request, language=None):
     if language is None:
         users = GithubUser.objects.all()
     elif language == 'None':
-        users = GithubUser.objects.filter(language='')
+        users = GithubUser.objects.filter(language__isnull=True)
     else:
         users = GithubUser.objects.filter(language__iexact=language)
     users = users.order_by('-public_repo_count')
-    languages = GithubUser.objects.values_list('language').exclude(language='').distinct().order_by('language')
+    languages = GithubUser.objects.values_list('language').exclude(language__isnull=True).distinct().order_by('language')
     languages = map(operator.itemgetter(0), languages)
     
     return render_to_response('list.html', 
